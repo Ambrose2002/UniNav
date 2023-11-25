e159881354c101ac8400d531a4056890462300a6
Ambrose <ambrose2002blay@gmail.com>
2023-11-25 16:25:48 -0500

    delete unused files

--- email.py
+++
@@ -1,8 +0,0 @@
-from Google import Create_Service
-
-CLIENT_FILE = "credentials.json"
-API_NAME = "gmail"
-API_VERSION = "v1"
-SCOPES = ["https://mail.google.com/"]
-
-service = Create_Service(CLIENT_FILE, API_NAME, API_VERSION, SCOPES)
